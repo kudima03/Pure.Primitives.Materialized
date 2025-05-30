d2140085217bc7847a3b77ffcc166a28240b2467
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-30 22:06:47 +0300

    Implemented tests for MaterializedTime.

---
+++ src/Tests/Pure.Primitives.Tests/Time/MaterializedTimeTests.cs
@@ -0,0 +1,30 @@
+﻿using Pure.Primitives.Date;
+using Pure.Primitives.Time;
+
+namespace Pure.Primitives.Tests.Time;
+
+using Date = Primitives.Date.Date;
+
+public sealed record MaterializedTimeTests
+{
+    [Fact]
+    public void MaterializeCorrectly()
+    {
+        Random random = new Random();
+        IReadOnlyCollection<DateOnly> randomDates = Enumerable.Range(0, 1000).Select(_ => DateOnly.MinValue.AddMonths(random.Next(0, 120000))).ToArray();
+
+        Assert.Equal(randomDates, randomDates.Select(x => new MaterializedDate(new Date(x)).Value));
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaterializedTime(new CurrentTime()).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaterializedTime(new CurrentTime()).ToString());
+    }
+}
