7b1e625d52976fafa825c343ce15319ceaadfec0
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-30 16:41:17 +0300

    Implemented tests for MaterializedDate

---
+++ src/Tests/Pure.Primitives.Tests/Date/MaterializedDateTests.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Date;
+
+namespace Pure.Primitives.Tests.Date;
+
+using Date = Primitives.Date.Date;
+
+public sealed record MaterializedDateTests
+{
+    [Fact]
+    public void MaterializeCorrectly()
+    {
+        DateOnly currentDate = DateOnly.FromDateTime(System.DateTime.Today);
+
+        Assert.Equal(currentDate, new MaterializedDate(new Date(currentDate)).Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaterializedDate(new CurrentDate()).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaterializedDate(new CurrentDate()).ToString());
+    }
+}
