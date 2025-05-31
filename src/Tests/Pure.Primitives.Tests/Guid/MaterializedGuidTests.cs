30d63fe93296973b2d1dabacb5ae04089ccaaf16
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-31 10:37:52 +0300

    Implemented tests for MaterializedGuid

---
+++ src/Tests/Pure.Primitives.Tests/Guid/MaterializedGuidTests.cs
@@ -0,0 +1,31 @@
+﻿using Pure.Primitives.Guid;
+
+namespace Pure.Primitives.Tests.Guid;
+
+using Guid = Primitives.Guid.Guid;
+
+public sealed record MaterializedGuidTests
+{
+    [Fact]
+    public void MaterializeIntCorrectly()
+    {
+        IEnumerable<System.Guid> guids = Enumerable.Range(0, 100).Select(_ => System.Guid.NewGuid()).ToArray();
+
+        IEnumerable<System.Guid> materializedGuids =
+            guids.Select(x => new Guid(x)).Select(x => new MaterializedGuid(x).Value);
+
+        Assert.Equal(guids, materializedGuids);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaterializedGuid(new Guid()).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaterializedGuid(new Guid()).ToString());
+    }
+}
